a707f63e9a74786d5ac73b129542783577508117
Andrew Catton <>
2018-10-15 14:52:47 +0000

    Added a test for demo purposes

---
+++ actions/end-to-end-tests/function.js
@@ -0,0 +1,20 @@
+function(ellipsis) {
+  const EllipsisApi = require('ellipsis-api');
+const api = new EllipsisApi(ellipsis);
+const test = require('tape');
+
+test.onFailure(ellipsis.error);
+test.onFinish(() => ellipsis.success("All passed!"));
+
+test('insult', function (t) {
+    t.plan(2);
+  
+    api.run({ trigger: "…insult @someone" }).then(res => {
+      console.log(`The results was: ${res}`);
+      const matches = res.match(/\@someone/g);
+      t.assert(matches);
+      t.equal(matches.length, 1);
+    });
+  
+});
+}
